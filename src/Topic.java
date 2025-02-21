036e47db1f999ea43ab7d84923ad592cb0495fcb
James B <110611129+Ging9999@users.noreply.github.com>
2025-02-21 17:08:44 +0000

    Update Topic.java
    added comments

--- src/Topic.java
+++ src/Topic.java
@@ -1,4 +1,8 @@
-enum Topic{
-    Python,
-    Java
+/**
+ * The Topic enum represents different programming languages or subjects
+ * that a flashcard can be categorized under.
+ */
+enum Topic {
+    Python, // Represents flashcards related to Python programming
+    Java    // Represents flashcards related to Java programming
 }
